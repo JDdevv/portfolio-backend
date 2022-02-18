2b79bbefd4746b2b45618edf02104d06f1f53118
JDdevv <juancastrozeese@gmail.com>
2022-02-18 11:12:09 -0300

    Added the first version of the project

---
+++ app.js
@@ -0,0 +1,19 @@
+require("dotenv").config()
+
+const express = require("express")
+const app = express()
+const path = require("path")
+
+app.use(express.static(path.join(__dirname,"build")))
+
+app.get("/" , ( req , res ) => {
+	res.sendFile(path.join(__dirname,"build","index.html"))
+})
+
+
+
+
+
+
+app.listen( process.env.PORT || 5000 , () => console.log( "server is running"))
+
